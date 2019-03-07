a3132406ff9584a916e6693e13f6696b4f7c3acb
beagbarreto <33846968+Beagbarreto@users.noreply.github.com>
2019-03-07 12:15:31 -0600

    Update recursion.js

--- src/recursion.js
+++ src/recursion.js
@@ -122,6 +122,10 @@ return powerOfTwo (n / 2);
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if(string === ""){
+    return "":
+  }
+  return reverse(string.substr(1)) + string.charAt(0);
 };
 
 // 10. Write a function that determines if a string is a palindrome.
